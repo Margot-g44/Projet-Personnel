f896fcf21dfd6ed2445f8be7dbe95ca21fa2fc9a
Margot Gouriou <margot.gouriou@gmail.com>
2025-07-08 17:53:01 +0200

    usestate émotions + historique

--- app/(tabs)/calendrier.tsx
+++ app/(tabs)/calendrier.tsx
@@ -1,14 +1,15 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Calendar } from 'react-native-calendars';
+import { Calendar } from "react-native-calendars";
 
 export default function Calendrier() {
   return (
     <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Calendrier</ThemedText>
-        <Calendar />
-      </ThemedView>
+      <ThemedText type="title">Calendrier</ThemedText>
+      <Calendar />
+    </ThemedView>
   );
 }
 
@@ -20,12 +21,20 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   titleContainer: {
-    flex:1, // aligner le titre Calendrier en haut et le centrer 
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // aligner le titre Calendrier en haut et le centrer
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 16,
     paddingTop: 50,
     gap: 8,
-
   },
 });
+
+const [selectedDate, setselectedDate] = useState(""); // création d'un état avec useState pour stocker la date sélectionnée par l'utilisateur
+// ex de fonctionnement : const [valeurActuelle, fonctionPourModifierValeur] = useState(valeurInitiale);
+// valeurActuelle -> C’est la donnée que tu veux stocker (ex. : une date, un objet, une liste, etc.)
+// fonctionPourModifierValeur -> Tu l’utilises pour mettre à jour cette donnée quand quelque chose change
+// valeurInitiale -> Ce que cette donnée vaut au départ, avant toute interaction
+
+const [historique, setHistorique] = useState([]); // création d'un état pour stocker l'historique des dates sélectionnées
+// historique -> C’est un tableau qui va contenir toutes les dates que l’utilisateur a sélectionnées
