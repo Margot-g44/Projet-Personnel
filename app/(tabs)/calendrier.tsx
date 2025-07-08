29a1d4835d369954b64c4b8b0ae0f8d1e719c973
Margot Gouriou <margot.gouriou@gmail.com>
2025-07-08 18:13:40 +0200

    affichage date sélectionnée

--- app/(tabs)/calendrier.tsx
+++ app/(tabs)/calendrier.tsx
@@ -4,12 +4,27 @@ import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Calendar } from "react-native-calendars";
 
+
 export default function Calendrier() {
+const [selectedDate, setselectedDate] = useState(""); // création d'un état avec useState pour stocker la date sélectionnée par l'utilisateur
+// ex de fonctionnement : const [valeurActuelle, fonctionPourModifierValeur] = useState(valeurInitiale);
+// valeurActuelle -> C’est la donnée que tu veux stocker (ex. : une date, un objet, une liste, etc.)
+// fonctionPourModifierValeur -> Tu l’utilises pour mettre à jour cette donnée quand quelque chose change
+// valeurInitiale -> Ce que cette donnée vaut au départ, avant toute interaction
+
+const [historique, setHistorique] = useState([]); // création d'un état pour stocker l'historique des dates sélectionnées
+// historique -> C’est un tableau qui va contenir toutes les dates que l’utilisateur a sélectionnées
   return (
     <ThemedView style={styles.titleContainer}>
       <ThemedText type="title">Calendrier</ThemedText>
-      <Calendar />
+    <Calendar
+      onDayPress={(day) => {
+        setselectedDate(day.dateString);
+    }}
+    />
+    <ThemedText>{selectedDate}</ThemedText>
     </ThemedView>
+    
   );
 }
 
@@ -30,11 +45,9 @@ const styles = StyleSheet.create({
   },
 });
 
-const [selectedDate, setselectedDate] = useState(""); // création d'un état avec useState pour stocker la date sélectionnée par l'utilisateur
-// ex de fonctionnement : const [valeurActuelle, fonctionPourModifierValeur] = useState(valeurInitiale);
-// valeurActuelle -> C’est la donnée que tu veux stocker (ex. : une date, un objet, une liste, etc.)
-// fonctionPourModifierValeur -> Tu l’utilises pour mettre à jour cette donnée quand quelque chose change
-// valeurInitiale -> Ce que cette donnée vaut au départ, avant toute interaction
 
-const [historique, setHistorique] = useState([]); // création d'un état pour stocker l'historique des dates sélectionnées
-// historique -> C’est un tableau qui va contenir toutes les dates que l’utilisateur a sélectionnées
+
+
+
+
+
